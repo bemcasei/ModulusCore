209a3df21e5dfacdaf5ff1eb8c5f657e71384996
William Hoffmann <30557425+willhoffmann@users.noreply.github.com>
2018-09-07 15:21:42 -0300

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -3,12 +3,27 @@
  * Configuration for ModulusCore module
  */
 
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 namespace ModulusCore;
 
 return [
     'controller_plugins' => [
         'factories' => [
             'cookie' => Factory\CookieFactory::class
+        ]
+    ],
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ . '_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [dirname(__DIR__) . '/src/Entity']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ]
+            ]
         ]
     ],
 ];
